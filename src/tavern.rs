101cf7d0f1332f503cc4d3d8cfcb41dcc5ed4097
DESKTOP-MNG36UR\Alberto <acorzomate@gmail.com>
2023-05-29 18:31:36 +0200

    resolved an issue with the result and renaming some variables

--- src/tavern.rs
+++ src/tavern.rs
@@ -1,27 +1,29 @@
-use std::io;
+use std::{io, error::Error};
+
+use serde::Serialize;
 
 use crate::heroes::*;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct Tavern {
     name: String,
-    pub people: Vec<Hero>,
+    pub heroes: Vec<Hero>,
 }
 
 impl Tavern {
     pub fn new(name: &str) -> Self {
         Self {
             name: String::from(name),
-            people: Vec::new(),
+            heroes: Vec::new(),
         }
     }
 
     pub fn add(&mut self, hero: Hero) {
-        self.people.push(hero);
+        self.heroes.push(hero);
     }
 
     pub fn show_heroes(&mut self) {
-        for hero in self.people.iter_mut() {
+        for hero in self.heroes.iter_mut() {
             //hero.describe();
             println!("{:#?}", hero)
         }
@@ -37,13 +39,13 @@ impl Tavern {
         //con lo que coge la posición del index exacto donde hay un caso positivo dentro de una sola línea
         //genio de idea
         //update: usando un Option, devolvemos la posición con un Some(usize) y en caso contrario con un None, evitando panics
-        let find_hero: Option<usize> = self.people.iter().position(|hero| hero.get_hero_name() == name);
+        let find_hero: Option<usize> = self.heroes.iter().position(|hero| hero.get_hero_name() == name);
 
         //Usamos un match para devolver un resultado u otro dependiendo de si encuentra algo o no, pero evitando el
         //código haciendo PANIC
         match find_hero {
             Some(usize) => {
-                self.people.remove(usize);
+                self.heroes.remove(usize);
                 println!("\nEl héroe {} ha sido echado por: {}\n", name, kick_motive);
                 },
             None => println!("No existe un héroe con ese nombre"),
@@ -51,14 +53,19 @@ impl Tavern {
     }
 
     fn find_hero(&self, name: &str) -> Option<usize> {
-        //let hero = self.people.iter().position(|hero| hero.get_hero_name() == name);
-        let find_hero = self.people.iter().position(|hero| hero.get_hero_name() == name);
+        //let hero = self.heroes.iter().position(|hero| hero.get_hero_name() == name);
+        let find_hero = self.heroes.iter().position(|hero| hero.get_hero_name() == name);
         find_hero
     }
 
     //Añadida capacidad de imprimir todo lo que contiene la taberna
-    pub fn write_json_tavern(&self) {    
-        std::fs::write("dark_tavern.json", serde_json::to_string_pretty(&self.people).expect("Error"));
+    pub fn write_json_tavern(&self) {
+        let filename = "dark_tavern.json";    
+        let read = std::fs::write(&filename, serde_json::to_string_pretty(&self).unwrap());
+        match read {
+            Ok(_) => println!("\nwriting of {} is successful", filename.to_uppercase()),
+            Err(_) => println!("\nwriting has failed"),
+        }
     }
 
     pub fn create_characters(&mut self) {
@@ -94,7 +101,7 @@ impl Tavern {
             io::stdin().read_line(&mut class).expect("Error reading CLASS");
 
             let new_hero = (Hero::new(name.trim(), race.trim(), weapon.trim(), class.trim()));
-            self.people.push(new_hero);
+            self.heroes.push(new_hero);
             
             if counter >= number_heroes || number_heroes == counter { break };
             counter += 1;

2e03272bb9af952280962dd72798f3a09b727618
DESKTOP-MNG36UR\Alberto <acorzomate@gmail.com>
2023-06-09 02:05:00 +0200

    Added deserializing and some more!

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,23 @@
+use std::{fs::File, io::BufReader};
+
+use heroes::Hero;
+use serde_json::Error;
+use tavern::Tavern;
+
 pub mod heroes;
 pub mod tavern;
+
+pub fn read_json_tavern(path: &str) {
+    let file = std::fs::read_to_string(path).expect("Error reading file");
+    let final_json: Tavern = serde_json::from_str(&file).expect("Error deserializing JSON");
+    println!("{:#?}", final_json);
+}
+
+pub fn check_file(path: &str) -> bool {
+    let result = File::open(path);
+
+    match result {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
